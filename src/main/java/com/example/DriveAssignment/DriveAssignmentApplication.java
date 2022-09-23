63685fb5cc15d2b5a8bb80afdd64401782a7270a
Mariya Josh <mariya.josh@medly.com>
2022-09-23 17:16:18 +0530

    Project setup

---
+++ src/main/java/com/example/DriveAssignment/DriveAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.DriveAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DriveAssignment/DriveAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DriveAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
