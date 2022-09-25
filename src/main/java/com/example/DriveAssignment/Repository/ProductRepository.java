572aa4e404bd6412d282a70580f9e951e104a4a2
Mariya Josh <mariya.josh@medly.com>
2022-09-25 15:47:29 +0530

    Add in-memory DB for integration tests and Add test for repository

--- src/main/java/com/example/DriveAssignment/Repository/ProductRepository.java
+++ src/main/java/com/example/DriveAssignment/Repository/ProductRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
     @Query(
---
+++ src/test/java/com/example/DriveAssignment/Repository/ProductRepositoryIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.example.DriveAssignment.Repository;
+
+import com.example.DriveAssignment.Model.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest()
+public class ProductRepositoryIntegrationTest {
+    @Autowired
+    private ProductRepository productRepository;
+    Product product = new Product(1, "RENT", "RENT", "", 24, 0, 0, 0.0, 0.0, null, "RENT", "SARADA PHARMACY");
+    List<Product> productList = new ArrayList<>();
+    @Test
+    public void saveProducts(){
+        productList.add(product);
+        productRepository.saveAll(productList);
+        productRepository.findById(1).get();
+        Assertions.assertEquals(productRepository.findById(1).get(), product);
+    }
+
+    @Test
+    public void getProducts(){
+        productList.add(product);
+        Page<Product> page =  productRepository.getProducts(null, null, null, null);
+        Assertions.assertEquals(page.getTotalElements(), 1);
+        Assertions.assertEquals(page.getContent(), productList);
+    }
+}
